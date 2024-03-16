d14db3fa5c975bc20c0b7ca139a3073610620909
awindest <william.indest@gmail.com>
2024-03-16 08:31:18 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<style>
+@font-face {
+    font-family: 'Averox';
+    src: url("/fonts/Averox-webfont.woff2") format("woff2");
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+}
+@font-face {
+    font-family: 'formadjrtext';
+    src: url("/fonts/formadjrtext-regular-webfont.woff2") format("woff2");
+    font-style: normal;
+    /* font-weight: 500; */
+    font-display: swap;
+}
+@font-face {
+    font-family: 'Manifold';
+    src: url("/fonts/ManifoldExtendedCF-Regular.woff2") format("woff2");
+    font-style: normal;
+    /* font-weight: 500; */
+    font-display: swap;
+}
+
+</style>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p> FormadjrText</p>
+<p class='extended1'> Manifold Extended</p>
+
+<style>
+    h1 {
+        font-family: 'Averox'
+    }
+    p {
+        font-family: 'formadjrtext'
+
+    }    
+    .extended1 {
+        font-family: 'Manifold'
+
+    }
+</style>
